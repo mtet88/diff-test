220e279399ddae7d780261419526c49ded5b5340
mauricio-torresmejia <mauricio.torresmejia@justeattakeaway.com>
2024-11-08 18:35:34 +0100

    Fix workflow

--- .github/actions/add_numbers/scripts/add.py
+++ .github/actions/add_numbers/scripts/add.py
@@ -10,18 +10,14 @@ def main():
         num1 = float(sys.argv[1])
         num2 = float(sys.argv[2])
         total = num1 + num2
-
-        # Retrieve GITHUB_OUTPUT and write the result to it
-        github_output_path = os.getenv('GITHUB_OUTPUT')
-        if github_output_path:
-            with open(github_output_path, 'a') as github_output:
-                github_output.write(f"total={total}\n")
-        else:
-            print("::error::GITHUB_OUTPUT environment variable not found.")
-            sys.exit(1)
+        set_output(total, 'total')
     except ValueError:
         print("::error::Inputs must be valid numbers.")
         sys.exit(1)
+
+def set_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print(f'{name}={value}', file=fh)
 
 if __name__ == "__main__":
     main()
